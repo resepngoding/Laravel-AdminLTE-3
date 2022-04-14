8aa9422478376bcf9ad608054864772b1c529db4
resepngoding <resepngoding@gmail.com>
2022-04-14 23:39:42 +0700

    fix layouts component

--- resources/views/admin/dashboard.blade.php
+++ resources/views/admin/dashboard.blade.php
@@ -1,34 +1,43 @@
 <x-app-layout>
-
-@section('title', 'Dashboard')
-
-@section('content')
+@push('styles')
+    
+@endpush
+<div class="content-header">
+    <div class="container-fluid">
+      <div class="row mb-2">
+        <div class="col-sm-6">
+          <h1 class="m-0">Dashboard</h1>
+        </div>
+        <div class="col-sm-6">
+          <ol class="breadcrumb float-sm-right">
+            <li class="breadcrumb-item"><a href="#">Home</a></li>
+            <li class="breadcrumb-item active">Dashboard</li>
+          </ol>
+        </div>
+      </div>
+    </div>
+  </div>
 <div class="row">
     <div class="col-md-12">
       <div class="card">
         <div class="card-header">
           <h5 class="card-title">Card Title</h5>
         </div>
-        <!-- /.card-header -->
         <div class="card-body">
           <div class="row">
               {{-- CARD CONTENT --}}
           </div>
-          <!-- /.row -->
         </div>
-        <!-- ./card-body -->
         <div class="card-footer">
           <div class="row">
           {{-- CARD FOOTER CONTENT --}}
           </div>
-          <!-- /.row -->
-        </div>
-        <!-- /.card-footer -->
       </div>
-      <!-- /.card -->
     </div>
-    <!-- /.col -->
   </div>
-  <!-- /.row -->
+
+  @push('js')
+     
+  @endpush
 
 </x-app-layout>
--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -1,51 +1,27 @@
 <!DOCTYPE html>
 <html lang="en">
 <head>
-    <x-partials.head/>
+<x-partials.head/>
  <livewire:styles/>
 </head>
 <body class="hold-transition dark-mode sidebar-mini layout-fixed layout-navbar-fixed layout-footer-fixed">
 <div class="wrapper">
-
-  <!-- Navbar -->
   <nav class="main-header navbar navbar-expand navbar-dark">
     <x-partials.navbar/>
   </nav>
-  <!-- /.navbar -->
 
-  <!-- Main Sidebar Container -->
   <aside class="main-sidebar sidebar-dark-primary elevation-4">
-  <x-partials.sidebar/>
-    <!-- /.sidebar -->
+    <x-partials.sidebar/>
   </aside>
 
-  <!-- Content Wrapper. Contains page content -->
   <div class="content-wrapper">
-    <!-- Content Header (Page header) -->
-    <div class="content-header">
-      <div class="container-fluid">
-        <div class="row mb-2">
-          <div class="col-sm-6">
-            <h1 class="m-0">@yield('title')</h1>
-          </div><!-- /.col -->
-          <div class="col-sm-6">
-            <ol class="breadcrumb float-sm-right">
-              <li class="breadcrumb-item"><a href="#">Home</a></li>
-              <li class="breadcrumb-item active">@yield('title')</li>
-            </ol>
-          </div><!-- /.col -->
-        </div><!-- /.row -->
-      </div><!-- /.container-fluid -->
-    </div>
-    <!-- /.content-header -->
-
-    <!-- Main content -->
+    @yield('content-header')
     <section class="content">
       <div class="container-fluid">
         {{ $slot }}
-      </div><!--/. container-fluid -->
+      </div>
     </section>
-    <!-- /.content -->
+
   </div>
   <!-- /.content-wrapper -->
 
